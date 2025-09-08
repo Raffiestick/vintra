cc2196373825046624ee1134ecd9cdd290e67578
Your Name <zofia1971@gmail.com>
2025-09-08 02:24:17 +0000

    ⚠ Cross origin request detected from 3000-firebase-studio-1756843660830.

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    // This is required for App Hosting to work with the App Router.
-    outputFileTracingExcludes: {
-      '*': [
-        './node_modules/@swc/core-linux-x64-gnu',
-        './node_modules/@swc/core-linux-x64-musl',
-        './node_modules/@esbuild/linux-x64',
-      ],
-    },
-    // This allowslist is required for the development environment to work correctly.
-    allowedDevOrigins: ['https://*.cloudworkstations.dev'],
+    allowedDevOrigins: ["*.cloudworkstations.dev"],
   },
 };
 
