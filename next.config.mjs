4ee48ceb776301941f403f1ee9821f2270c3586e
Stick <zofia1971@gmail.com>
2025-09-16 00:41:54 +0000

    This didn't fix the issue.  What is going on here?

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // !! WARN !!
     ignoreBuildErrors: true,
+  },
+  webpack: (config) => {
+    // See https://webpack.js.org/configuration/resolve/#resolvealias
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'sharp$': false,
+      'onnxruntime-node$': false,
+    }
+    return config
   },
 };
 
