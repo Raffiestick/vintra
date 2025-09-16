b43d39cdc1b9bae1d9c324367f2d0a24b1b1233b
Stick <zofia1971@gmail.com>
2025-09-16 05:00:15 +0000

    studio-2512513726:~/studio{backup-20250916-0433-split-fns}$ cd functions

--- src/functions/lib/index.js
+++ src/functions/lib/index.js
@@ -1,8 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.startDocParse = exports.generateJacketPacket = exports.generateBillOfSale = exports.generateJacketInvoice = exports.attachStagedDocToJacket = exports.createJacketsForInvoice = exports.createJacketFromUnit = exports.startInvoiceParse = exports.generateJacketId = exports.manageDealerApplication = exports.signInWithCustomToken = void 0;
 // Re-export all your functions from small modules
-export { signInWithCustomToken, manageDealerApplication, generateJacketId } from "./modules/auth";
-export { startInvoiceParse, createJacketFromUnit, createJacketsForInvoice } from "./modules/staging";
-export { attachStagedDocToJacket } from "./modules/docs";
-export { generateJacketInvoice, generateBillOfSale } from "./modules/invoices";
-export { generateJacketPacket } from "./modules/packet";
+var auth_1 = require("./modules/auth");
+Object.defineProperty(exports, "signInWithCustomToken", { enumerable: true, get: function () { return auth_1.signInWithCustomToken; } });
+Object.defineProperty(exports, "manageDealerApplication", { enumerable: true, get: function () { return auth_1.manageDealerApplication; } });
+Object.defineProperty(exports, "generateJacketId", { enumerable: true, get: function () { return auth_1.generateJacketId; } });
+var staging_1 = require("./modules/staging");
+Object.defineProperty(exports, "startInvoiceParse", { enumerable: true, get: function () { return staging_1.startInvoiceParse; } });
+Object.defineProperty(exports, "createJacketFromUnit", { enumerable: true, get: function () { return staging_1.createJacketFromUnit; } });
+Object.defineProperty(exports, "createJacketsForInvoice", { enumerable: true, get: function () { return staging_1.createJacketsForInvoice; } });
+var docs_1 = require("./modules/docs");
+Object.defineProperty(exports, "attachStagedDocToJacket", { enumerable: true, get: function () { return docs_1.attachStagedDocToJacket; } });
+var invoices_1 = require("./modules/invoices");
+Object.defineProperty(exports, "generateJacketInvoice", { enumerable: true, get: function () { return invoices_1.generateJacketInvoice; } });
+Object.defineProperty(exports, "generateBillOfSale", { enumerable: true, get: function () { return invoices_1.generateBillOfSale; } });
+var packet_1 = require("./modules/packet");
+Object.defineProperty(exports, "generateJacketPacket", { enumerable: true, get: function () { return packet_1.generateJacketPacket; } });
 // Keep placeholder so CLI won't try to delete if you had this name deployed.
-export { startDocParse } from "./placeholder";
+var placeholder_1 = require("./placeholder");
+Object.defineProperty(exports, "startDocParse", { enumerable: true, get: function () { return placeholder_1.startDocParse; } });
+//# sourceMappingURL=index.js.map
