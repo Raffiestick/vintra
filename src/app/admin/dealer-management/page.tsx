10c0b4807451e7a1bff838872afc040ffefc538d
Stick <zofia1971@gmail.com>
2025-09-16 08:44:48 +0000

    Unhandled Runtime Error ChunkLoadError: Loading chunk _app-pages-browser

--- src/app/admin/dealer-management/page.tsx
+++ src/app/admin/dealer-management/page.tsx
@@ -1,12 +1,37 @@
 'use client';
 
 import dynamic from 'next/dynamic';
+import { Skeleton } from '@/components/ui/skeleton';
 
 const DealerManagementClient = dynamic(() => import('./DealerManagementClient'), { 
   ssr: false,
-  // You can add a loading component here if you wish
-  // loading: () => <p>Loading...</p> 
+  loading: () => <DealerManagementSkeleton />
 });
+
+function DealerManagementSkeleton() {
+  return (
+    <div className="space-y-4">
+      <Skeleton className="h-8 w-1/4" />
+      <Skeleton className="h-4 w-1/2" />
+      <div className="border rounded-md">
+        <div className="flex items-center justify-between p-4 border-b">
+          <Skeleton className="h-6 w-1/6" />
+          <Skeleton className="h-6 w-1/6" />
+          <Skeleton className="h-6 w-1/6" />
+          <Skeleton className="h-6 w-1/6" />
+        </div>
+        <div className="p-4 space-y-4">
+          <div className="flex items-center justify-between">
+            <Skeleton className="h-5 w-1/6" />
+            <Skeleton className="h-5 w-1/6" />
+            <Skeleton className="h-5 w-1/6" />
+            <Skeleton className="h-5 w-1/6" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default function Page() {
   return <DealerManagementClient />;
