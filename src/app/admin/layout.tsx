bb671afade4134b54c17467c266c980e8980b6b3
Stick <zofia1971@gmail.com>
2025-09-18 21:54:18 +0000

    I get this when trying to log in as admin:
    Unhandled Runtime Error
    Err

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,9 +1,5 @@
 "use client";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-export const fetchCache = "force-no-store";
-
 import AdminShell from "@/components/admin/AdminShell";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
