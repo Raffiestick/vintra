be7c1436989943657f2b6aa6663dc4575d6fbf1b
Stick <zofia1971@gmail.com>
2025-09-18 22:01:59 +0000

    something keeps happening everytime i run npm run dev it break the landi

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,10 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   darkMode: ["class"],
   content: [
-    "./src/app/**/*.{ts,tsx}",
-    "./src/components/**/*.{ts,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     container: {
