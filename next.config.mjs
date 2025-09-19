e8a9050e935c47b4079e0ca4faa5aa502679841b
Your Name <zofia1971@gmail.com>
2025-09-19 14:52:11 +0000

    i have this error in landing page:
    Invalid src prop (https://picsum.ph

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // This is required for the cloud editor to work.
-  // We can remove this when we deploy to production.
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   devIndicators: {
     allowedDevOrigins: [
       "4000-firebase-studio-1756843660830.cluster-pgviq6mvsncnqxx6kr7pbz65v6.cloudworkstations.dev",
